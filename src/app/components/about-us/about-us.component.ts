3d014fff7474ddc46bb1f55f4b390edada9dab30
SachokS <stanislav12312@mail.ru>
2019-09-21 19:12:01 +0300

    add yandex maps

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import ymaps from "ymaps";
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  public map: any;
+  public storeLocation;
+
   constructor() { }
 
   ngOnInit() {
+    ymaps
+      .load()
+      .then(maps => {
+        this.map = new maps.Map('yandex-map', {
+          center: [53.935632, 27.583230],
+          zoom: 14
+        });
+        this.storeLocation = new maps.GeoObject({
+          geometry: {
+            type: "Point",
+            coordinates:  [53.935632, 27.583230]
+          }
+        });
+        this.map.geoObjects.add(this.storeLocation);
+      })
+      .catch(error => console.log('Failed to load Yandex Maps', error));
+
+
   }
 
 }
