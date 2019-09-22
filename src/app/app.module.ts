ed1192946b5b0aa91f710430e74e28c8288c3f7b
SachokS <stanislav12312@mail.ru>
2019-09-23 00:39:18 +0300

    change header, add social icons, error-404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import {ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+
 @NgModule({
   declarations: [
     AppComponent,
