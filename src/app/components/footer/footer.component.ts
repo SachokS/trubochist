a132d54f394c43dff14f0f28ebd528869299ed23
SachokS <stanislav12312@mail.ru>
2019-09-24 19:51:28 +0300

    change color, icon, info about person

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,4 +12,24 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
   }
 
+  public goToSocialNetwork(nameOfNetwork: string) {
+    switch (nameOfNetwork) {
+      case 'vk':
+        window.open('https://vk.com/sachok13');
+        break;
+      case 'telegram':
+        window.open(' https://t.me/Daymovoy');
+        break;
+      case 'facebook':
+        window.open('https://www.facebook.com/profile.php?id=100028524320869');
+        break;
+      case 'instagram':
+        window.open('https://www.instagram.com/konstantinkhodnevich/');
+        break;
+      case 'youtube':
+        window.open('https://vk.com/sachok13');
+        break;
+    }
+  }
+
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  public goToSocialNetwork(nameOfNetwork: string) {
+    switch (nameOfNetwork) {
+      case 'vk':
+        window.open('https://vk.com/sachok13');
+        break;
+      case 'telegram':
+        window.open(' https://t.me/Daymovoy');
+        break;
+      case 'facebook':
+        window.open('https://www.facebook.com/profile.php?id=100028524320869');
+        break;
+      case 'instagram':
+        window.open('https://www.instagram.com/konstantinkhodnevich/');
+        break;
+      case 'youtube':
+        window.open('https://vk.com/sachok13');
+        break;
+    }
   }
 
 }
