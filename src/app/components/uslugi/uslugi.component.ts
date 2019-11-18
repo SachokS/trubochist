162218c7013ea9ee9c0a4590577fbbbcaab620be
SachokS <stanislav12312@mail.ru>
2019-11-19 00:15:00 +0300

    add empty uslugi page

---
+++ src/app/components/uslugi/uslugi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uslugi',
+  templateUrl: './uslugi.component.html',
+  styleUrls: ['../../../assets/styles/uslugi.component.less']
+})
+export class UslugiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
